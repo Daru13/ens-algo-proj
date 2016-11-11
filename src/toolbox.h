27b94d083b9da83df3e6d9708810df853b0982ec
Daru13 <daru14@gmail.com>
2016-11-11 19:35:20 +0100

    Better organization of files + improved Makefile

---
+++ src/toolbox.h
@@ -0,0 +1,14 @@
+//------------------------------------------------------------------------------
+// TOOLBOX
+//------------------------------------------------------------------------------
+
+#ifndef _TOOLBOX_H
+#define _TOOLBOX_H
+
+// Basic boolean type, for clearer types
+typedef char bool;
+
+#define true  1
+#define false 0
+
+#endif

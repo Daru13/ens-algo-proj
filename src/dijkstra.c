4ab24f2a31d8e8b5711c9bb42265d6748229c5c6
Daru13 <daru14@gmail.com>
2016-12-21 17:08:52 +0100

    Modified Dijkstra's algorithm, which is now (apparently) working

--- src/dijkstra.c
+++ src/dijkstra.c
@@ -55,27 +55,34 @@ int* dijkstraNaive (Graph* g, int s) // O(#A²)
 
 #define INF_LENGTH -1
 
-int extractMinimumNaive (int* queue, int* lengths, int nb_elements)
+int extractMinimumNaive (bool* seen, int* lengths, int nb_vertexes)
 {
 	int i;
-	int current_vertex = queue[0];
+	int min = -1;
 
 	// Find the minimum
-	for (i = 1; i < nb_elements; i++)
-		if (lengths[queue[i]] != INF_LENGTH
-		&&	lengths[queue[i]]  < lengths[current_vertex])
+	for (i = 0; i < nb_vertexes; i++)
+		if (lengths[i] != INF_LENGTH
+		&& !seen[i])
+		
 		{
-			int sauvegarde = current_vertex;
-			current_vertex = queue[i];
-			queue[i] = sauvegarde;
+			if (min == -1)
+			{
+			min = i;
+			} 
+			else 
+			{
+				if (lengths[min] > lengths[i])
+				{
+				min = i;
+				}
+			}
+
 		}
-	// Shift the table elements
-	 for (int j = i; j < nb_elements - 1; j++)
-		queue[j] = queue[j + 1];
 	
 
 
-	return current_vertex;
+	return min;
 }
 
 int* dijkstraNaive (Graph* g, int s) // O(#A²)
@@ -91,43 +98,32 @@ int* dijkstraNaive (Graph* g, int s) // O(#A²)
 		lengths[i] = INF_LENGTH;
 	}
 
-	int priority_queue[g->nb_vertexes];
-	priority_queue[0] = s;
 	lengths[s] 		  = 0;
 
-	int priority_queue_nb_elts = 1;
 
 	// Main loop (over the priority queue structure)
-	while (priority_queue_nb_elts > 0)
+	for (int i = 0; i<g->nb_vertexes;i++)
 	{
 		// The element with the smallest distance is extracted
-		int min_elt = extractMinimumNaive(priority_queue,
-			lengths, priority_queue_nb_elts);
-		priority_queue_nb_elts--;
-		// If it is the first time the vertex is encountered...
-		if (! seen[min_elt])
+		int min_elt = extractMinimumNaive(seen,
+			lengths, g->nb_vertexes);
+		seen[min_elt] = true;
+		// Iteration over all min_elt's neighbours
+		Edge* current_edge = g->edges[min_elt];
+		while (current_edge != NULL)
 		{
-			seen[min_elt] = true;
-
-			// Iteration over all min_elt's neighbours
-			Edge* current_edge = g->edges[min_elt];
-			while (current_edge != NULL)
+			// Each neighbour is added to the priority structure
+			int neighbour = current_edge->destination;
+			// Improve the lengths if possible
+			int weight = current_edge->weight;
+			if (lengths[neighbour] == INF_LENGTH
+			||  lengths[neighbour] > lengths[min_elt] + weight)
 			{
-				// Each neighbour is added to the priority structure
-				int neighbour = current_edge->destination;
-				priority_queue[priority_queue_nb_elts] = neighbour;
-				priority_queue_nb_elts++;
-
-				// Improve the lengths if possible
-				int weight = current_edge->weight;
-				if (lengths[neighbour] == INF_LENGTH
-				||  lengths[neighbour] > lengths[min_elt] + weight)
-					lengths[neighbour] = lengths[min_elt] + weight;
-
-				current_edge = current_edge->next;
+				lengths[neighbour] = lengths[min_elt] + weight;
 			}
+			current_edge = current_edge->next;
 		}
 	}
-
 	return lengths;
 }
+
